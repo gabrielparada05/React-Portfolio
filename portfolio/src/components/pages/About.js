4c414aee390e8b6af49f8fc54aaaf80116691ff6
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-06 23:15:13 -0400

    out and favicon done

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import Typical from 'react-typical'
+import logo from '../../images/logo-png.png'
 
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-1 py-4 md:flex-row flex-col items-center">
-        <div className="flex items-center flex-wrap justify-center">
-          <div className='flex flex-row justify-center w-auto'>
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white flex justify-center">
-              Hi, I am Gabriel!
-            </h1>
-          </div>
-
+      <div className="container flex  items-center h-screen flex-col px-1 py-4">
+      <img src={logo} className='w-60'/>
+        <div className="flex flex-col text-center">
+          
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I am Gabriel!
+          </h1>
           <p className="text-justify">
-            As a seasoned Manager in Social Services with years of experience leading teams and managing
-            complex projects, I am now pursuing a new career path in Web Development. With a strong foundation in leadership,
-            strategic planning, and problem-solving. I have completed a rigorous Web Development Bootcamp through the
-            University of Toronto. This immersive program provided me with a solid understanding of full-stack web development,
-            including front-end development with HTML, CSS, and JavaScript, as well as back-end development with Node.js, Express, and MongoDB.
-            Through individual and team projects, I gained practical experience developing responsive and dynamic websites and web applications.
-            The skills and knowledge I gained from this program have prepared me well for a successful transition to a career in Web Development.
+           
+            <Typical
+              loop={Infinity}
+              wrapper="P"
+              steps={[
+                'I am a former Manager in Social Services',
+                1000,
+                'I have strong foundation in leadership, strategic planning, and problem-solving,',
+                1000,
+                'I am pursuing a new career path in Web Development,',
+                1000, 
+                'I have a solid understanding of full-stack web development,',
+                1000,
+                'I am excited about the opportunity to learn and share my knowledge with you.'
+              ]}
+            />
           </p>
         </div>
       </div>
     </section>
   );
 }
+
+
+
+
