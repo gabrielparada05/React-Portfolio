132fc790a1f7d75c515b433d7f397a8678b10efa
Gabriel Parada <gabrielparada05@gmail.com>
2023-09-22 15:38:03 -0400

    deploying github

--- portfolio/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-
-
-
-const App = () => <div className='text-gray-400 body-font'><PortfolioContainer /> </div>;
-
-export default App;
