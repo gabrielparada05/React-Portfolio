1119e0dbea91fac8fd1f1ce6f8bcca9d05e3122b
Gabriel Parada <gabrielparada05@gmail.com>
2023-10-04 11:41:54 -0400

    styling footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -6,10 +6,10 @@ import '../styles/style.css';
 export default function FooterRender() {
 
   return (
-    <footer className="foot container bg-sky-300 text-gray-400 py-4 text-center flex justify-center flex-col ">
+    <footer className="foot bg-sky-300 text-gray-400 py-4 text-center w-full flex justify-center flex-col ">
       <div className="flex justify-center mb-2">
         <a href="https://github.com/gabrielparada05" className="text-blue-500 underline hover:text-blue-700">
-          <img src={Github} alt="Logo Github" className="w-8 h-8 mr-4" />
+          <img src={Github} alt="Logo Github" className="w-7 h-7 mr-4" />
         </a>
         <a href="https://www.linkedin.com/in/gabriel-parada-moreno-78b8083b/" className="text-blue-500 underline hover:text-blue-700">
           <img src={Linkedin} alt="Logo Linkedin" className="w-8 h-15 ml-4" />
